8f7a556556862359321b4b0dd85340c1cd66dc7b
vitaliiiiin <nesterenkov90@gmail.com>
2021-03-04 16:34:43 +0200

    Added a new field _data and its properties Data

--- Lab1/Lab1/Node.cs
+++ Lab1/Lab1/Node.cs
@@ -2,6 +2,12 @@
 {
     public class Node<T>
     {
+        private T _data;
+        public T Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
         
     }
 }
