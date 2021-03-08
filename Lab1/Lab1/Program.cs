edee58c8d47b875c92a605248683a81107c238d7
vitaliiiiin <nesterenkov90@gmail.com>
2021-03-08 18:31:04 +0200

    Added new test of LinkedList into Main

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,53 +6,11 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            // LinkedList<object> listTest = new LinkedList<object>();
-            //
-            // object i = 0;
-            // listTest.AddItem(i);
-            //
-            // object b = 5;
-            // listTest.AddItem(b);
-            //
-            // object c = 2;
-            // listTest.AddItem(c);
-            //
-            // listTest.PrintList();
-            //
-            // Console.WriteLine("\n");
-            //
-            // LinkedList<int> list2 = new LinkedList<int>();
-            //
-            // list2.AddItem(1);
-            // list2.AddItem(0);
-            // list2.AddItem(3);
-            // list2.AddItem(2);
-            // list2.AddItem(0);
-            //
-            // list2.PrintList();
-            // Console.WriteLine();
-            // list2.DeleteItem(0);
-            // list2.PrintList();
-            //
-            // Console.WriteLine();
-            // Console.WriteLine(list2.Search(1));
-            // Console.WriteLine();
-            //
-            //
-            // LinkedList<string> list3 = new LinkedList<string>();
-            //
-            // list3.AddItem("z");
-            // list3.AddItem("b");
-            // list3.AddItem("k");
-            // list3.AddItem("a");
-            // list3.AddItem("a");
-            //
-            // list3.PrintList();
-
-            //creating a new list
+            //creating a new list of ints
             LinkedList<int> listInts = new LinkedList<int>();
             
             //adding new nodes into the list
+            Console.WriteLine("after adding new nodes into the empty list");
             listInts.AddItem(9);
             listInts.AddItem(2);
             listInts.AddItem(8);
@@ -61,23 +19,88 @@ namespace Lab1
             listInts.AddItem(1);
             listInts.AddItem(8);
 
-            //printing the list
+            //printing the list and its size
             listInts.PrintList();
+            Console.WriteLine("\nits size: " + listInts.ListSize());
+            
+            Console.WriteLine();
             
             //deleting items from the list
             listInts.DeleteItem(8);
             listInts.DeleteItem(0);
             listInts.DeleteItem(7);
 
-            Console.WriteLine("\n");
-            
-            //printing the list
+            //printing the list and its size
+            Console.WriteLine("after deleting items from the list");
             listInts.PrintList();
+            Console.WriteLine("\nits size: " + listInts.ListSize());
 
-            Console.WriteLine("\n");
+            Console.WriteLine();
             
-            
+            //searching an item in the list
+            Console.WriteLine("searching an item in the list");
+            int itemToFind = 2;
+            Console.WriteLine(itemToFind + ": " + listInts.Search(itemToFind));
+            itemToFind = 8;
+            Console.WriteLine(itemToFind + ": " + listInts.Search(itemToFind));
+            itemToFind = 9;
+            Console.WriteLine(itemToFind + ": " + listInts.Search(itemToFind));
+            itemToFind = 11;
+            Console.WriteLine(itemToFind + ": " + listInts.Search(itemToFind));
 
+            Console.WriteLine();
+            
+            //checking if the list is empty
+            Console.WriteLine("is the list empty? " + listInts.IsEmpty());
+            
+            Console.WriteLine("\n=================");
+            
+            //creating a new list of objects
+            LinkedList<object> listObjects = new LinkedList<object>();
+
+            object i = 0;
+            listObjects.AddItem(i);
+            i = 10;
+            listObjects.AddItem(i);
+            i = 5;
+            listObjects.AddItem(i);
+            i = 8;
+            listObjects.AddItem(i);
+            i = 3;
+            listObjects.AddItem(i);
+
+            //printing the list and its size
+            listObjects.PrintList();
+            Console.WriteLine("\nits size: " + listObjects.ListSize());
+            
+            //searching an item in the list
+            Console.WriteLine("searching an item in the list");
+            itemToFind = 10;
+            Console.WriteLine(itemToFind + ": " + listObjects.Search(itemToFind));
+            itemToFind = 8;
+            Console.WriteLine(itemToFind + ": " + listObjects.Search(itemToFind));
+            itemToFind = 0;
+            Console.WriteLine(itemToFind + ": " + listObjects.Search(itemToFind));
+            itemToFind = 11;
+            Console.WriteLine(itemToFind + ": " + listObjects.Search(itemToFind));
+            
+            Console.WriteLine();
+            
+            //deleting items from the list
+            listObjects.DeleteItem(8);
+            listObjects.DeleteItem(0);
+            listObjects.DeleteItem(7);
+
+            //printing the list and its size
+            Console.WriteLine("after deleting items from the list");
+            listObjects.PrintList();
+            Console.WriteLine("\nits size: " + listObjects.ListSize());
+
+            Console.WriteLine();
+            
+            //checking if the list is empty
+            Console.WriteLine("is the list empty? " + listObjects.IsEmpty());
+            
             Console.ReadLine();
         }
     }
